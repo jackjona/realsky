5d3a131a619c5e7b783e46b28269f41bfeafa0fe
Jack Jona <65679392+jackjona123@users.noreply.github.com>
2022-04-25 18:38:29 -0400

    production

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'ca1594d4c4aa174afb1913f40900c92c';
+
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY
+        }
+    } );
+
+    return data;
+}
